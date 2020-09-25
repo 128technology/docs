b627ce9aac394dd6ad41227b3bce6ca8f175779f
Ian Chamberlain <ichamberlain@128technology.com>
2020-09-25 12:30:20 -0400

    Add installer/initializer docs from i95
    Ref DOCS-120, DOCS-154, IN-64, I95-37431

--- sidebars.js
+++ sidebars.js
@@ -13,20 +13,20 @@ module.exports = {
     "Introduction": [
       "intro_getting_started",
       "intro_installation",
-        "intro_system_reqs",
-        "intro_prepare_OS",
-        "intro_installation_installer",
+      "intro_system_reqs",
+      "intro_prepare_OS",
+      "intro_installation_installer",
 
       "intro_downloading_iso",
       "intro_installation_bootable_media",
       "intro_creating_bootable_usb",
       "intro_installation_otp_iso",
       "intro_ztp",
-      
+
       "intro_installation_aws",
       "intro_installation_quickstart_aws",
-      "intro_installation_azure",  
-      
+      "intro_installation_azure",
+
       "intro_upgrading",
       "intro_rollback",
     ],
@@ -124,6 +124,12 @@ module.exports = {
     ],
     "CLI Reference": [
       "cli_reference",
+    ],
+    "Installer/Initializer Reference": [
+      "installer_cli_reference",
+      "installer_preferences",
+      "initializer_cli_reference",
+      "initializer_preferences",
     ],
     "Plugins": [
       "plugin_intro",
