1f3f51a7b575945a2967be34edbfa37100a82ed9
Chris <christophert@juniper.net>
2022-08-12 11:55:26 -0400

    adding release notes, feature info for password policies, and fixing a typo in the Config element guide.

--- docs/releases.table.js
+++ docs/releases.table.js
@@ -6,6 +6,13 @@ import React from "react";
 // ----------------------------------------------------------------------------
 export const releases = [
    {
+    version: '6.0.1',
+    url: 'release_notes_128t_6.0#release-601-12',
+    releaseDate: 'August 15, 2022',
+    endOfMaintenance: 'April 20, 2023',
+    endOfSupport: 'October 20, 2023',
+   },
+   {
     version: '5.5.3',
     url:'release_notes_128t_5.5#release-553-4',
     releaseDate: 'August 11, 2022',
