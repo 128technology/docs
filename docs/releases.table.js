e5056749be18a912b30ec42555a8c26a06b9adc1
Chris <christophert@juniper.net>
2023-08-02 14:05:30 -0400

    restoring 4.x refs

--- docs/releases.table.js
+++ docs/releases.table.js
@@ -301,6 +301,16 @@ export const releases = [
     releaseDate: 'July 7, 2021',
   },
   {
+    version: '4.5.10',
+    url: '/docs/release_notes_128t_4.5#release-4510',
+    releaseDate: 'June 8, 2021',
+  },
+  {
+    version: '4.5.9',
+    url: '/docs/release_notes_128t_4.5#release-459',
+    releaseDate: 'May 20, 2021 ',
+  },
+  {
     version: '5.1.3',
     url: '/docs/release_notes_128t_5.1#release-513',
     releaseDate: 'May 17, 2021',
@@ -316,6 +326,11 @@ export const releases = [
     releaseDate: 'April 30, 2021',
   },
   {
+    version: '4.5.8',
+    url: '/docs/release_notes_128t_4.5#release-458',
+    releaseDate: 'April 28, 2021',
+  },
+  {
     version: '5.1.1',
     url: '/docs/release_notes_128t_5.1#release-511',
     releaseDate: 'April 12, 2021',
@@ -326,14 +341,254 @@ export const releases = [
     releaseDate: 'April 12, 2021',
   },
   {
+    version: '4.5.7',
+    url: '/docs/release_notes_128t_4.5#release-457',
+    releaseDate: 'April 12, 2021',
+  },
+  {
+    version: '4.5.6',
+    url: '/docs/release_notes_128t_4.5#release-456',
+    releaseDate: 'March 26, 2021',
+  },
+  {
     version: '5.1.0',
     url: '/docs/release_notes_128t_5.1#release-5.1',
     releaseDate: 'March 15, 2021',
   },
   {
+    version: '4.3.12',
+    url: '/docs/release_notes_128t_4.3#release-4311',
+    releaseDate: 'March 12, 2021 ',
+  },
+  {
+    version: '4.5.5',
+    url: '/docs/release_notes_128t_4.5#release-455',
+    releaseDate: 'February 10, 2021',
+  },
+  {
     version: '5.0.0',
     url: '/docs/release_notes_128t_5.0#release-5.0',
     releaseDate: 'December 18, 2020',
+  },
+  {
+    version: '4.5.4',
+    url: '/docs/release_notes_128t_4.5#release-454',
+    releaseDate: 'December 16, 2020',
+  },
+  {
+    version: '4.5.3',
+    url: '/docs/release_notes_128t_4.5#release-453',
+    releaseDate: 'November 25, 2020',
+  },
+  {
+    version: '4.2.9',
+    url: '/docs/release_notes_128t_4.2#release-429',
+    releaseDate: 'November 20, 2020',
+  },
+  {
+    version: '4.3.11',
+    url: '/docs/release_notes_128t_4.3#release-4311',
+    releaseDate: 'November 13, 2020',
+  },
+  {
+    version: '4.3.10',
+    url: '/docs/release_notes_128t_4.3#release-4310',
+    releaseDate: 'October 20, 2020',
+  },
+  {
+    version: '4.5.2',
+    url: '/docs/release_notes_128t_4.5#release-452',
+    releaseDate: 'October 13, 2020',
+  },
+  {
+    version: '4.5.1',
+    url: '/docs/release_notes_128t_4.5#release-451',
+    releaseDate: 'September 16, 2020',
+  },
+  {
+    version: '4.4.2',
+    url: '/docs/release_notes_128t_4.3#release-442',
+    releaseDate: 'September 3, 2020',
+  },
+  {
+    version: '4.3.9',
+    url: '/docs/release_notes_128t_4.3#release-439',
+    releaseDate: 'August 12, 2020',
+  },
+  {
+    version: '4.5.0',
+    url: '/docs/release_notes_128t_4.5#release-450',
+    releaseDate: 'July 23, 2020',
+  },
+  {
+    version: '4.4.1',
+    url: '/docs/release_notes_128t_4.3#release-441',
+    releaseDate: 'July 10, 2020',
+  },
+  {
+    version: '4.3.8',
+    url: '/docs/release_notes_128t_4.3#release-438',
+    releaseDate: 'June 26, 2020',
+  },
+  {
+    version: '4.3.7',
+    url: '/docs/release_notes_128t_4.3#release-437',
+    releaseDate: 'June 9, 2020',
+  },
+  {
+    version: '4.2.8',
+    url: '/docs/release_notes_128t_4.2#release-428',
+    releaseDate: 'June 5, 2020',
+  },
+  {
+    version: '4.3.6',
+    url: '/docs/release_notes_128t_4.3#release-436',
+    releaseDate: 'May 28, 2020',
+  },
+  {
+    version: '4.1.10',
+    url: '/docs/release_notes_128t_4.1#release-4110',
+    releaseDate: 'May 28, 2020',
+  },
+  {
+    version: '4.3.5',
+    url: '/docs/release_notes_128t_4.3#release-435',
+    releaseDate: 'May 22, 2020',
+  },
+  {
+    version: '4.4.0',
+    url: '/docs/release_notes_128t_4.4#release-440',
+    releaseDate: 'May 19, 2020',
+  },
+  {
+    version: '4.3.4',
+    url: '/docs/release_notes_128t_4.3#release-434',
+    releaseDate: 'May 1, 2020',
+  },
+  {
+    version: '4.2.7',
+    url: '/docs/release_notes_128t_4.2#release-427',
+    releaseDate: 'May 1, 2020',
+  },
+  {
+    version: '4.1.9',
+    url: '/docs/release_notes_128t_4.1#release-419',
+    releaseDate: 'May 1, 2020',
+  },
+  {
+    version: '4.3.3',
+    url: '/docs/release_notes_128t_4.3#release-433',
+    releaseDate: 'April 12, 2020',
+  },
+  {
+    version: '4.3.2',
+    url: '/docs/release_notes_128t_4.3#release-432',
+    releaseDate: 'April 10, 2020',
+  },
+  {
+    version: '4.2.6',
+    url: '/docs/release_notes_128t_4.2#release-426',
+    releaseDate: 'April 8, 2020',
+  },
+  {
+    version: '4.2.5',
+    url: '/docs/release_notes_128t_4.2#release-425',
+    releaseDate: 'March 26, 2020',
+  },
+  {
+    version: '4.3.1',
+    url: '/docs/release_notes_128t_4.3#release-431',
+    releaseDate: 'March 6, 2020 ',
+  },
+  {
+    version: '4.1.8',
+    url: '/docs/release_notes_128t_4.1#release-418',
+    releaseDate: 'Februray 28, 2020',
+  },
+  {
+    version: '4.2.4',
+    url: '/docs/release_notes_128t_4.2#release-424',
+    releaseDate: 'February 14, 2020',
+  },
+  {
+    version: '4.3.0',
+    url: '/docs/release_notes_128t_4.3#release-430',
+    releaseDate: 'February 8, 2020',
+  },
+  {
+    version: '4.2.3',
+    url: '/docs/release_notes_128t_4.2#release-423',
+    releaseDate: 'January 27, 2020',
+  },
+  {
+    version: '4.1.7',
+    url: '/docs/release_notes_128t_4.1#release-417',
+    releaseDate: 'January 8, 2020',
+  },
+  {
+    version: '4.2.2',
+    url: '/docs/release_notes_128t_4.2#release-422',
+    releaseDate: 'December 20, 2019',
+  },
+  {
+    version: '4.2.1',
+    url: '/docs/release_notes_128t_4.2#release-421',
+    releaseDate: 'December 13, 2019',
+  },
+  {
+    version: '4.1.6',
+    url: '/docs/release_notes_128t_4.1#release-416',
+    releaseDate: 'December 6, 2019',
+  },
+  {
+    version: '4.2.0',
+    url: '/docs/release_notes_128t_4.2#release-420',
+    releaseDate: 'November 21, 2019',
+  },
+  {
+    version: '4.1.5',
+    url: '/docs/release_notes_128t_4.1#release-415',
+    releaseDate: 'July 24, 2019',
+  },
+  {
+    version: '4.1.4',
+    url: '/docs/release_notes_128t_4.1#release-414',
+    releaseDate: 'June 9, 2019',
+  },
+  {
+    version: '4.1.3',
+    url: '/docs/release_notes_128t_4.1#release-413',
+    releaseDate: 'April 26, 2019',
+  },
+  {
+    version: '4.1.2',
+    url: '/docs/release_notes_128t_4.1#release-412',
+    releaseDate: 'April 16, 2019',
+  },
+  {
+    version: '4.1.1',
+    url: '/docs/release_notes_128t_4.1#release-411',
+    releaseDate: 'March 5, 2019',
+  },
+  {
+    version: '4.1.0',
+    url: '/docs/release_notes_128t_4.1#release-410',
+    releaseDate: 'February 7, 2019',
+  },
+  {
+    version: '4.0.1',
+    url: '/docs/release_notes_128t_4.0#release-401',
+    releaseDate: 'January 22, 2019',
+  },
+  {
+    version: '4.0.0',
+    url: '',
+    releaseDate: 'December 18, 2018',
+  },
+  {
+    version: '3.2.8',
+    url: '',
+    releaseDate: 'November 12, 2018',
   },
 ];
 
