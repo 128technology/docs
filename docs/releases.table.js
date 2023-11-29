1d6d91a070b0204385c42eb1610a9eb7a5f12272
Chris <christophert@juniper.net>
2023-11-29 13:40:24 -0500

    creating 5.5.12 release notes

--- docs/releases.table.js
+++ docs/releases.table.js
@@ -5,6 +5,11 @@ import React from "react";
 // New entries should be placed at the top.
 // ----------------------------------------------------------------------------
 export const releases = [
+    {
+    version: '5.5.12',
+    url:'/docs/release_notes_128t_5.5#release-5512-5',
+    releaseDate: 'August 21, 2023',
+  },
   {
     version: '6.2.0',
     url: 'release_notes_128t_6.2#release-620-39',
