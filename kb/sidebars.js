18caa870c5de638b4202210ec478df407246f29c
Chris <christophert@juniper.net>
2024-05-03 09:33:15 -0400

    trying to add a sidebar toc to the KB doc.

---
+++ kb/sidebars.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "kb": {
+    "Knowledg Base Articles": [
+      "about_128t",
+      "about_svr_savings",
+      "about_releases",
+      "about_support_policy",
+      "about_supported_platforms",
+      "about_certified_platforms",
+      "about_security_policy",
+      "CONTRIBUTING",
+    ],
+  },
+};
