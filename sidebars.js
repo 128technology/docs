33b898de270584d4fc568d00e9ab7aa1f757e226
Chris <christophert@juniper.net>
2024-10-29 16:50:23 -0400

    correct filename

--- sidebars.js
+++ sidebars.js
@@ -324,7 +324,7 @@ module.exports = {
         "type": "category",
         "label": "How To",
         "items": [
-          "howto_bios_upgrade",
+          "howto_update_bios",
           "how_to_local_config_override",
           "howto_extend_gui_nav",
           "howto_lte",
