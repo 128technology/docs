901ce0e69555e797d28298928a3dcebc737e8eda
Chris <christophert@juniper.net>
2025-01-16 17:12:47 -0500

    these are the latest revisions to the 6.1.12 release notes. there was some local corruption in several other files that got added. Please verify the changes in the reviewed and updated docs.

--- docs/releases.table.js
+++ docs/releases.table.js
@@ -7,6 +7,11 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 // ----------------------------------------------------------------------------
 export const releases = [
   {
+    version: '6.1.12',
+    url: '/docs/release_notes_128t_6.1#release-6112-12-lts',
+    releaseDate: 'January 16, 2025',
+  },
+  {
     version: '6.3.3',
     url: '/docs/release_notes_128t_6.3#release-633-40r2',
     releaseDate: 'January 9, 2025',
