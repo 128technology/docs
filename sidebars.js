ae3eae7b77a11f1c6d527f3713488b7d82347205
Chris <christophert@juniper.net>
2026-09-14 12:36:52 -0400

    interim check in

--- sidebars.js
+++ sidebars.js
@@ -233,9 +233,9 @@ module.exports = {
                   "deploy_vmware_hub_router_pci",
                   "deploy_vmware_hub_router_config",
                   "deploy_appendix_vmware_hub_router",
-              ],
-            },
-            {
+                ],
+              },
+              {
                 "type": "category",
                 "label": "VMware Spoke Router",
                 "items": [
@@ -245,8 +245,20 @@ module.exports = {
                   "deploy_vmware_router_pci",
                   "deploy_vmware_router_config",
                   "deploy_appendix_vmware_router",
-              ],
-            },
+                ],
+              },
+              {
+                "type": "category",
+                "label": "Azure Hub and Spoke Routers",
+                "items": [
+                  "deploy_azure_hub_spoke_overview",
+                  "deploy_azure_hub_spoke_hub_vm",
+                  "deploy_azure_hub_spoke_spoke_vm",
+                  "deploy_azure_hub_spoke_vmbus",
+                  "deploy_azure_hub_spoke_config",
+                  "deploy_appendix_azure_hub_spoke",
+                ],
+              },
           ],
         },
       ],
